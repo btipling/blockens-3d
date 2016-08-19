baa911a269eabd3240223b4852d94f8d4e96f725
Bjorn Tipling <bjorn@ambientchill.com>
2016-08-19 05:13:09 -0700

    Initial project.

---
+++ blockens-3d/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  blockens-3d
+//
+//  Created by Bjorn Tipling on 8/19/16.
+//  Copyright (c) 2016 apphacker. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
---
+++ blockens-3dTests/blockens_3dTests.swift
@@ -0,0 +1,36 @@
+//
+//  blockens_3dTests.swift
+//  blockens-3dTests
+//
+//  Created by Bjorn Tipling on 8/19/16.
+//  Copyright (c) 2016 apphacker. All rights reserved.
+//
+
+import XCTest
+@testable import blockens_3d
+
+class blockens_3dTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
